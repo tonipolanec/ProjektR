ee7a7279821d0894eed458bd430efaf460c2dc0f
matijapavlovic8 <101288349+matijapavlovic8@users.noreply.github.com>
2022-12-16 23:27:34 +0100

    Convolutional network built.

---
+++ Colorizer.py
@@ -0,0 +1,1 @@
+#U principu main
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,10 +1,42 @@
 import tensorflow as tf
-from tensorflow import keras
-from keras import layers
+from keras.applications import Xception
+from tensorflow.python.keras.layers import Input, Conv2D, UpSampling2D, RepeatVector, Reshape
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras import backend
 
 
-class Model:
-    def __init__(self, ):
+def create_model(input):
+    model = Conv2D(64, (3, 3), activation='relu', padding='same', strides=2)(input)
+    model = Conv2D(128, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(128, (3, 3), activation='relu', padding='same', strides=2)(model)
+    model = Conv2D(256, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(256, (3, 3), activation='relu', padding='same', strides=2)(model)
+    model = Conv2D(512, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(512, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(256, (3, 3), activation='relu', padding='same')(model)
+
+    embeding = Reshape(28, 28, 1000)(RepeatVector(28 * 28)(Input(shape=(1000,), name='embeding')))
+
+    model = backend.concatenate([model, embeding], axis=3)
+    model = Conv2D(256, (1, 1), activation='relu', padding='same')(model)
+
+    model = Conv2D(256, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(128, (3, 3), activation='relu', padding='same')(model)
+    model = UpSampling2D((2, 2))(model)
+    model = Conv2D(64, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(64, (3, 3), activation='relu', padding='same')(model)
+    model = UpSampling2D((2, 2))(model)
+    model = Conv2D(32, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(16, (3, 3), activation='relu', padding='same')(model)
+    model = Conv2D(2, (3, 3), activation='tanh', padding='same')(model)
+    model = UpSampling2D((2, 2))(model)
+
+    return model
+
+
+
+
+
 
 
 
